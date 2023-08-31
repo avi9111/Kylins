57b75e98c686c3c3eee82e7e6c38c6e363d73ac4
avi9111@163.com <avi9111@163.com>
2023-09-01 02:37:54 +0800

    第二次提交，加上Foam贴图

--- assets/stoneStudio/StudioPlane.ts
+++ assets/stoneStudio/StudioPlane.ts
@@ -1,4 +1,5 @@
-import { _decorator, Component, Node, RenderTexture,director,Camera,MeshRenderer} from 'cc';
+import { _decorator, Component, Node, RenderTexture,director,Camera,MeshRenderer
+    ,Vec3,v3,quat,Quat} from 'cc';
 import { Utils } from '../resources/kylins_water_surface/scripts/Utils';
 const { ccclass, property } = _decorator;
 
@@ -20,6 +21,67 @@ export class StudioPlane extends Component {
     update(deltaTime: number) {
         
     }
+    private _tmpV3_0: Vec3 = v3();
+    private _tmpV3_1: Vec3 = v3();
+    private _tmpV3_N: Vec3 = v3();
+    private _tmpQuat: Quat = quat();
+    getMirrorPoint(out: Vec3, p: Vec3, n: Vec3, d: number): Vec3 {
+        if (out == null) {
+            out = new Vec3();
+        }
+        let dist = Vec3.dot(p, n) - d;
+        //tmp = N
+        let tmp = v3(n);
+        //tmp = 2.0 * dist * N
+        tmp.multiplyScalar(2.0 * dist);
+        // out = p - 2.0 * dist * N
+        Vec3.subtract(out, p, tmp);
+        return out;
+    }
+    protected lateUpdate(dt: number): void {
+        if(!this.mainCamera)
+            return;
+
+        if(this._reflectionCamera && this._reflectionCamera.node.active)
+        {
+            let target = this.mainCamera.node;
+            // let source = this._reflectionCamera.node;
+
+            // let planeWorldPos = this._mesh.node.worldPosition;
+            // Vec3.transformQuat(this._tmpV3_N, Vec3.FORWARD, this._mesh.node.worldRotation);
+            // let n = this._tmpV3_N;
+            // n.negative();
+            // n.normalize();
+
+            // let d = Vec3.dot(planeWorldPos, n);
+
+            // //position.
+            // this.getMirrorPoint(this._tmpV3_0, target.worldPosition, n, d);
+            // source.worldPosition = this._tmpV3_0;
+
+            // //forward
+            // Vec3.transformQuat(this._tmpV3_0, Vec3.FORWARD, target.worldRotation);
+            // this._tmpV3_0.set(target.forward);
+            // this._tmpV3_0.normalize();
+            // this.getMirrorPoint(this._tmpV3_0, this._tmpV3_0, n, 0);
+            // this._tmpV3_0.normalize();
+            // this._tmpV3_0.negative();
+
+            // //up
+            // Vec3.transformQuat(this._tmpV3_1, Vec3.UP, target.worldRotation);
+            // this._tmpV3_1.normalize();
+            // this.getMirrorPoint(this._tmpV3_1, this._tmpV3_1, n, 0);
+            // this._tmpV3_1.normalize();
+
+            // //calculate rotation with view(forward) and up directions
+            // Quat.fromViewUp(this._tmpQuat, this._tmpV3_0, this._tmpV3_1);
+            // source.worldRotation = this._tmpQuat;
+
+            //sync worldScale
+            this._reflectionCamera.node.setWorldScale(target.worldScale);
+            Utils.syncCameraParameters(this._reflectionCamera, this.mainCamera);
+        }
+    }
 
     initReflection():RenderTexture{
         let texture = Utils.createRenderTexture(1)
@@ -32,6 +94,7 @@ export class StudioPlane extends Component {
         this._reflectionCamera.visibility = this.mainCamera.visibility
         this._reflectionCamera.targetTexture = texture
         this._reflectionCamera.name = 'Reflection Camera'
+        node.name = 'Reflection Camera'
 
         return texture
     }
