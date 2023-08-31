046a0f62aa75872055a18d4ee3cc5cd1abc70978
luyuchi <luyuchi@guangzhoumaitian.com>
2023-08-31 18:42:38 +0800

    第一次提交？？

---
+++ assets/stoneStudio/StudioPlane.ts
@@ -0,0 +1,15 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('StudioPlane')
+export class StudioPlane extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
+
